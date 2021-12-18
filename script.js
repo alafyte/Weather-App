7d6fe4eb82c61aff980136fc6c92348486f72748
alafyte <xlebxlebxleb0508@gmail.com>
2021-12-18 18:46:36 +0300

    The page was translated on russian

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let weather = {
     "apiKey": "5b2128d63c305eb8b79256d63d130931",
     fetchWeather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey).then((response) => response.json()).then((data) => this.displayWeather(data));
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=ru&units=metric&appid=" + this.apiKey).then((response) => response.json()).then((data) => this.displayWeather(data));
     },
     displayWeather: function(data)
     {
@@ -9,12 +9,12 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".city").innerText = "Погода в городе " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+        document.querySelector(".humidity").innerText = "Влажность: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Скорость ветра: " + speed + "км/ч";
         document.querySelector(".weather").classList.remove("loading");
     },
     search: function() {
@@ -31,4 +31,4 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
     }
 });
 
-weather.fetchWeather("Minsk");
+weather.fetchWeather("Минск");
